5daf077a565203431e6723fb558e86b3cb03f48b
Bravetomato <princeps2nd@gmail.com>
2023-03-05 15:20:20 +0900

    add nodemon

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world!!!');
+    res.send('Hello world!!!????');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About!');
 });
 
 app.listen(port, () => {
